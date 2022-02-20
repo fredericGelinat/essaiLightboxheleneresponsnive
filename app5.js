c25fe9ca70cb270fe0cca0f5ed76175ab490740b
fredericGelinat <fredericgelinat@gmail.com>
2022-02-20 14:35:38 +0100

    suppr prototype files

--- app5.js
+++
@@ -1,9 +0,0 @@
-
- 
- function change(imageaaffiche){
-document.getElementById('coucou').src=imageaaffiche
-}
-
-function change(texteaafficher){
-    document.getElementById('coucou').firstChild.nodeValue=texteaafficher
-    } 
